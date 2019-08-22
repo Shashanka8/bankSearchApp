a8ede453a81ca63a6b92bc198c51b54759da1f1e
Shashanka8 <nayak.shashanka97@gmail.com>
2019-08-22 14:58:23 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+import { Observable, from, Subject } from 'rxjs';
+
+import { Bankdetails } from './classes/bankdetails';
+import { HttpClient } from '@angular/common/http';
+
+// import { Cacheable } from 'ngx-cacheable';
+
+// const articleNotifier = new Subject();
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BankSearchApp';
+  
+  listBank: any;
+  filterBank: Bankdetails[];
+  selectedLoc: string = 'BANGALORE';
+
+  constructor(private httpclient: HttpClient) { }
+
+  private _search: string;
+  get search(): string {
+    return this._search;
+  }
+  set search(value: string) {
+    this._search = value;
+    this.filterBank = this.filterBanks(value);
+  }
+
+  filterBanks(serachStr: string) {
+    return this.listBank.filter(bank =>
+      bank.branch.toLowerCase().indexOf(serachStr.toLowerCase()) !== -1);
+  }
+
+  getDetails() {
+    this.httpclient.get("https://vast-shore-74260.herokuapp.com/banks?city="  + this.selectedLoc)
+    .subscribe( response => {
+      this.listBank = response;
+    });
+    this.filterBank = this.listBank;    
+  }
+
+  selectChangeHandler(event: any) {
+    this.selectedLoc = event.target.value;
+    this.getDetails();
+  }
+
+  ngOnInit() {
+    this.getDetails();
+  }
+  
 }
